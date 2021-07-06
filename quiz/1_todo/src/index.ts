1ff750bf873e95ff4d5c29bd28ca914bed5f2b61
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-06 20:59:14 +0900

    type 정하기

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,7 +1,9 @@
-let todoItems;
+let todoItems: object[];
+//배열안에 객체가 있기 때문에 타입은 object[]
 
 // api
-function fetchTodoItems() {
+//fetchTodoItems 함수의 반환 값은 object[]
+function fetchTodoItems(): object[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -11,21 +13,26 @@ function fetchTodoItems() {
 }
 
 // crud methods
-function fetchTodos() {
+function fetchTodos(): object[] {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo) {
+//리턴값이 없기 때문에 void로 설정
+function addTodo(todo:object):void {
   todoItems.push(todo);
 }
 
-function deleteTodo(index) {
+//index의 타입은 number
+//반환값은 void
+function deleteTodo(index: number):void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
-  todo.done = true;
+//index는 number
+//반환값은 없어서 void
+function completeTodo(index: number, todo: object): void {
+  todo.done = true; //모르겠음
   todoItems.splice(index, 1, todo);
 }
 
@@ -35,14 +42,15 @@ function logFirstTodo() {
 }
 
 function showCompleted() {
-  return todoItems.filter(item => item.done);
+  return todoItems.filter(item:Array => item:Array.done);
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
 function addTwoTodoItems() {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  function addTodo(id=4,: Array<string>)
+  function addTodo(영어공부: Array<string>)
 }
-
 // NOTE: 유틸 함수
 function log() {
   console.log(todoItems);
@@ -51,3 +59,5 @@ function log() {
 todoItems = fetchTodoItems();
 addTwoTodoItems();
 log();
+
+
