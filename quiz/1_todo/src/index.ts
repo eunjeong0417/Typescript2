0bf917d0e4d1f8c9de5b93d61d2d77a820d34fd4
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-06 22:52:40 +0900

    type 정의하기

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,9 +1,10 @@
-let todoItems: object[];
+let todoItems: {id: number, title: string, done: boolean}[];
 //배열안에 객체가 있기 때문에 타입은 object[]
 
 // api
 //fetchTodoItems 함수의 반환 값은 object[]
-function fetchTodoItems(): object[] {
+//보다 구체적으로 type 정의함
+function fetchTodoItems(): {id: number, title: string, done: boolean}[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -19,7 +20,7 @@ function fetchTodos(): object[] {
 }
 
 //리턴값이 없기 때문에 void로 설정
-function addTodo(todo:object):void {
+function addTodo(todo:{id: number, title: string, done: boolean}):void {
   todoItems.push(todo);
 }
 
@@ -31,28 +32,38 @@ function deleteTodo(index: number):void {
 
 //index는 number
 //반환값은 없어서 void
-function completeTodo(index: number, todo: object): void {
-  todo.done = true; //모르겠음
+function completeTodo(index: number, todo: {id: number, title: string, done: boolean}): void {
+  todo.done = true; //todo 객체의 프로퍼티를 구체적으로 설정해주면 오류 사라짐.
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo(): object {
   return todoItems[0];
 }
 
-function showCompleted() {
-  return todoItems.filter(item:Array => item:Array.done);
+function showCompleted():object[] {
+  return todoItems.filter(item => item.done); //모르겠음
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+//반환값이 없어서 void
+function addTwoTodoItems(): void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
-  function addTodo(id=4,: Array<string>)
-  function addTodo(영어공부: Array<string>)
+  const item4:object = {
+    id: 4,
+    title: "nice",
+    done: true
+  }
+  addTodo(item4);
+  addTodo({
+    id: 5,
+    title: "ho",
+    done: false
+  })
 }
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
